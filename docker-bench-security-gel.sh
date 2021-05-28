cff2ea5282259bdbefccec8ce790cfab1a119f73
Fernando Karnagi <fernando@cyder.com.sg>
2021-05-28 08:51:52 +0800

    Updated sh

--- docker-bench-security-gel.sh
+++ docker-bench-security-gel.sh
@@ -7,4 +7,4 @@
 # Checks for section 4 and 5
 # --------------------------------------------------------------------------------------------
 
-docker-bench-security.sh -c check_4_all,check_5_all
+./docker-bench-security.sh -c check_4_all,check_5_all
